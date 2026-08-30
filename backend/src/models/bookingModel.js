0c22411bf60d3db45f627c7bd57fcd953841e038
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-08-31 01:56:14 +0530

    add booking model to the database

---
+++ backend/src/models/bookingModel.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from 'mongoose';
+
+const bookingSchema = new Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "User", // Links to the user model
+            required: true,
+        },
+        movie: {
+            type: Schema.Types.ObjectId,
+            ref: "Movie", // Links to the movie model
+            required: true,
+        },
+        seats: {
+            type: [String], // Array of seat identifiers
+            required: true,
+        },
+        totalPrice: {
+            type: Number,
+            required: true,
+        },
+        paymentMethod: {
+            type: String,
+            enum: ["Mastercard", "Visa", "Paypal"],
+            required: true,
+        },
+        PaymentStatus: {
+            type: String,
+            enum: ["PENDING", "COMPLETED", "FAILED"],
+            default: "COMPLETED",
+        }
+    },
+    { timestamps: true}
+);
+
+export const Booking = mongoose.model("Booking", bookingSchema);
