711be838da31fbff6569fccc6c6081401049a9d0
Dimalka Navod <dimalkanavod.dnw@gmail.com>
2026-09-23 02:27:08 +0530

    wire the routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
+router.get("/current", verifyJWT, getCurrentUser); // Must logged in to access
+router.post("/logout", loggeoutUser); 
+
 export default router;
+
+
